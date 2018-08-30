ac178b0cb96140fcefdd93ef48d5e3e0e3423dce
al_m <al_m@worksap.co.jp>
2018-08-30 18:12:09 +0800

    Update primitives - 5.2, 5.3

--- primitives/src/main/java/ReverseBits.java
+++ primitives/src/main/java/ReverseBits.java
@@ -1,11 +1,19 @@
 public class ReverseBits {
 
-    /*
-    5.3
-    */
+	/*
+	 * 5.3
+	 */
 
-    public static int reverse(int x) {
+	public static int reverse(int x) {
+		int wordSize = Integer.SIZE - 1;
+		int reverse = 0;
+		int mask = 1;
 
-        return 0;
-    }
+		for (int i = 0; i <= wordSize; i++) {
+			if (((x >>> i) & 0x1) == 1)
+				reverse ^= (mask << (wordSize - i));
+		}
+
+		return reverse;
+	}
 }
--- primitives/src/main/java/SwapBits.java
+++ primitives/src/main/java/SwapBits.java
@@ -5,7 +5,7 @@ public class SwapBits {
     */
 
     public static long swapBits(long x, int i, int j) {
-    	if (((x >> i) & 1) != ((x >> j) & 1)) {
+    	if (((x >>> i) & 1) != ((x >>> j) & 1)) {
     		long bitMask = (1L << i) ^ (1L << j);
     		return x ^ bitMask;
     	}
