2743e0103dbe5f17ffb910a4c9aaa3fe2487f99a
al_m <al_m@worksap.co.jp>
2018-08-31 23:59:01 +0800

    Update primitives - 5.4

--- primitives/src/main/java/FindClosestInteger.java
+++ primitives/src/main/java/FindClosestInteger.java
@@ -1,11 +1,19 @@
 public class FindClosestInteger {
 
-    /*
-    5.4
-    */
+	/*
+	 * 5.4
+	 */
 
-    public static long closestIntSameBitCount(long x) {
+	public static long closestIntSameBitCount(long x) {
+		final int bits = 63;
 
-        return 0;
-    }
+		for (int i = 0; i < bits; i++) {
+			if (((x >>> i) & 1) != ((x >>> (i + 1)) & 1)) {
+				x ^= (1L << i) | (1L << (i + 1));
+				break;
+			}
+		}
+
+		return x;
+	}
 }
