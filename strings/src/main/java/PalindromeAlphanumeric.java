2e71672317cf2859b9417a3387309f7f0762f45e
al_m <al_m@worksap.co.jp>
2018-09-18 00:30:53 +0800

    Update strings - 7.5

--- strings/src/main/java/PalindromeAlphanumeric.java
+++ strings/src/main/java/PalindromeAlphanumeric.java
@@ -1,11 +1,38 @@
 public class PalindromeAlphanumeric {
 
-    /*
-    7.5
-    */
+	/*
+	 * 7.5
+	 */
 
-    public static boolean isPalindrome(String input) {
+	public static boolean isPalindrome(String input) {
+		String inputLC = input.toLowerCase();
+		
+		int i = 0, j = inputLC.length() - 1;
 
-        return false;
-    }
+		while (i < j) {
+			char iChar = inputLC.charAt(i), jChar = inputLC.charAt(j);
+
+			while ((i < j) && !isLetterOrDigit(iChar)) {
+				i++;
+				iChar = inputLC.charAt(i);
+			}
+
+			while ((i < j) && !isLetterOrDigit(jChar)) {
+				j--;
+				jChar = inputLC.charAt(j);
+			}
+
+			if (iChar != jChar)
+				return false;
+
+			i++;
+			j--;
+		}
+
+		return true;
+	}
+
+	private static boolean isLetterOrDigit(char c) {
+		return (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+	}
 }
