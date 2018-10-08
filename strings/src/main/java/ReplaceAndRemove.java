2daab74f0973ad5c053bf82d69e0d990cf679a3f
al_m <al_m@worksap.co.jp>
2018-10-08 23:35:10 +0800

    Update strings - 7.3, 7.4

--- strings/src/main/java/ReplaceAndRemove.java
+++ strings/src/main/java/ReplaceAndRemove.java
@@ -1,12 +1,37 @@
 public class ReplaceAndRemove {
 
-    /*
-    7.4
-    */
+	/*
+	 * 7.4
+	 */
 
-    public static String replaceAndRemove(char[] s, int k) {
+	public static String replaceAndRemove(char[] s, int k) {
+		int writeIdx = 0, aCount = 0;
+		k = s.length;
 
-        return s.toString();
-    }
+		for (int i = 0; i < k; i++) {
+			if (s[i] != 'b' && ((int) s[i]) != 32)
+				s[writeIdx++] = s[i];
+
+			if (s[i] == 'a')
+				++aCount;
+		}
+
+		writeIdx--;
+		int currIdx = writeIdx;
+
+		writeIdx += aCount;
+
+		for (int i = currIdx; i >= 0; i--) {
+			if (s[i] == 'a') {
+				s[writeIdx--] = 'd';
+				s[writeIdx--] = 'd';
+			}
+			else {
+				s[writeIdx--] = s[i];
+			}
+		}
+
+		return new String(s);
+	}
 
 }
--- strings/src/main/java/SpreadsheetColumnEncoding.java
+++ strings/src/main/java/SpreadsheetColumnEncoding.java
@@ -1,13 +1,18 @@
 public class SpreadsheetColumnEncoding {
 
-    /*
-    7.3
-    */
+	/*
+	 * 7.3
+	 */
 
-    public static int decodeSpreadsheetColumn(String code) {
+	public static int decodeSpreadsheetColumn(String code) {
+		int result = 0, c = 0, base = 26;;
 
-        return 0;
-    }
+		for (int i = 0; i < code.length(); i++) {
+			c = code.charAt(i) - 'A' + 1;
+			result = result * base + c;
+		}
 
+		return result;
+	}
 
 }
