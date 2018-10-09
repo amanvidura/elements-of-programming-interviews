41c522ed3b9418e770dad23eb87233862883d9a2
al_m <al_m@worksap.co.jp>
2018-10-09 18:58:27 +0800

    Update strings - 7.6

--- strings/src/main/java/ReverseWordsInASentence.java
+++ strings/src/main/java/ReverseWordsInASentence.java
@@ -1,11 +1,18 @@
 public class ReverseWordsInASentence {
 
-    /*
-    7.6
-    */
+	/*
+	 * 7.6
+	 */
 
-    public static String reverseWordsInASentence(String input) {
+	public static String reverseWordsInASentence(String input) {
+		String[] inputArr = input.split(" ");
 
-        return "";
-    }
+		for (int i = 0, mid = inputArr.length / 2; i < mid; i++) {
+			String temp = inputArr[i];
+			inputArr[i] = inputArr[inputArr.length - i - 1];
+			inputArr[inputArr.length - i - 1] = temp;
+		}
+
+		return String.join(" ", inputArr);
+	}
 }
